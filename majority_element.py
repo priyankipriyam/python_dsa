5ab2eb10cf02ebd8649cc5b1152f876daa66f457
Priyanki Priyam Borgohain <priyankipriyam1912@gmail.com>
2025-07-24 01:04:53 +0530

    majority element

---
+++ majority_element.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mp = {}
+        for i in range(len(nums)):
+            ##hashmap code
+            if nums[i] in mp:
+                mp[nums[i]] += 1
+            else:
+                mp[nums[i]] = 1
+            if mp[nums[i]] * 2 > len(nums):
+                return nums[i]
+
+        return -1
+sol = Solution()
+print(sol.majorityElement([3, 2, 3]))  # Output: 3
+print(sol.majorityElement([2, 2, 1, 1, 1, 2, 2]))  # Output: 2
+print(sol.majorityElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  # Output: -1
