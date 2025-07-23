2dcc593bfffcb5f28b4916733beff3cb4c1a31d5
Priyanki Priyam Borgohain <priyankipriyam1912@gmail.com>
2025-07-24 01:08:16 +0530

    Majority_element

--- majority_element.py
+++ majority_element.py
@@ -15,6 +15,17 @@ class Solution(object):
                 return nums[i]
 
         return -1
+    
+# class Solution(object):
+#     def majorityElement(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         nums.sort()
+#         return nums[len(nums) // 2]
+    
+
 sol = Solution()
 print(sol.majorityElement([3, 2, 3]))  # Output: 3
 print(sol.majorityElement([2, 2, 1, 1, 1, 2, 2]))  # Output: 2
