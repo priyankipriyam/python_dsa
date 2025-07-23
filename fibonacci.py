7a043f25f7d7d0b916232abb413858e4454ed9d7
Priyanki Priyam Borgohain <priyankipriyam1912@gmail.com>
2025-07-24 01:38:10 +0530

    Fibonacci sequence

--- fibonacci.py
+++ fibonacci.py
@@ -33,10 +33,12 @@ class Solution(object):
         """
         memo = {}
         def helper(n):
-            if n == 0 or n == 1: return n
-            if n in memo: return memo[n]
+            if n == 0 or n == 1: 
+                return n
+            if n in memo: 
+                return memo[n]
             memo[n] = helper(n - 1) + helper(n - 2)
-            print('memo:', memo)
+            ##print('memo:', memo)
             return memo[n]
         return helper(n)
     
