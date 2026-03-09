5bb40fdae265783696f6dd67b582d68753fe973f
Priyanki Priyam Borgohain <priyankipriyam1912@gmail.com>
2026-03-09 22:57:21 +0530

    Updated questions on majority_element.py, pivotIndex.py and running_sum_of_1D_array.py.

--- majority_element.py
+++ majority_element.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
         mp = {}
         for i in range(len(nums)):
             ##hashmap code
@@ -16,12 +17,29 @@ class Solution(object):
 
         return -1
     
+
+#Boyer-Moore Algorithm
+
+        # count = 0
+        # candidate = None
+
+        # for num in nums:
+        #     if count == 0:
+        #         candidate = num
+
+        #     if num == candidate:
+        #         count += 1
+        #     else:
+        #         count -= 1
+        # return candidate
+    
 # class Solution(object):
 #     def majorityElement(self, nums):
 #         """
 #         :type nums: List[int]
 #         :rtype: int
 #         """
+# this solution is not optimal as it has O(nlogn) time complexity due to sorting, and O(n) space complexity while the Boyer-Moore algorithm has O(n) time complexity and O(1) space complexity.
 #         nums.sort()
 #         return nums[len(nums) // 2]
     
@@ -29,4 +47,4 @@ class Solution(object):
 sol = Solution()
 print(sol.majorityElement([3, 2, 3]))  # Output: 3
 print(sol.majorityElement([2, 2, 1, 1, 1, 2, 2]))  # Output: 2
-print(sol.majorityElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  # Output: -1
+print(sol.majorityElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  # Output: -1 (Boyer-Moore fails here as this array does not have a majority element)
--- running_sum_of_1D_array.py
+++ running_sum_of_1D_array.py
@@ -4,16 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        crs = 0
-        numsRs = []
-        for i in range(len(nums)):
-            crs = crs + nums[i]
-            numsRs.append(crs)
-        return numsRs
+        # crs = 0
+        # numsRs = []
+        # for i in range(len(nums)):
+        #     crs = crs + nums[i]
+        #     numsRs.append(crs)
+        # return numsRs
 
         # for i in range(1, len(nums)):
         #     nums[i] += nums[i-1]
         # return nums
+
+ 
+        rSum = 0
+        for i in range(len(nums)):
+            rSum = rSum + nums[i]
+            nums[i] = rSum
+        return nums
+        
 
 sol = Solution()
 print(sol.runningSum([1, 2, 3, 4]))  # Output: [1, 3, 6, 10]
